fa75efc6d7556478369442fe91f37dcba90c3094
thtrang68 <thtrang6806@gmail.com>
2025-03-23 00:13:56 +0700

    Add files via upload

---
+++ html.c
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> chào cậucậu CSS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Bài tập web của Trần Thùy Trang CSS</h1>
+    <p class="external-style">Đây là một đoạn văn có áp dụng external CSS.</p>
+</body>
+</html>
